e282d6edf9f733b50ae5b87c97814b6bd7bf9a2b
Aleksey Krasnov <neky@list.ru>
2021-11-25 02:20:09 +0500

    header

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
 
+export interface  Items {
+  name: string;
+  link: string;
+  active: boolean;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
   constructor() { }
+  public items: Items[]  = [
+    {
+      name: 'Каталог',
+      link: '1',
+      active: false,
+    },
+    {
+      name: 'Сделать заказ',
+      link: '1',
+      active: false,
+    },
+    {
+      name: 'Контакты',
+      link: '1',
+      active: false,
+    },
+  ];
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public dataService: DataService) { }
+  public item: DataService[] = [];
   ngOnInit(): void {
+    console.log(this.dataService.items[0].name)
   }
 
 }
